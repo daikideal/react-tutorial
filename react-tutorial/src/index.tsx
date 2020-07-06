3930140e741f1dc15b1285ebd82946f1227354be
daikideal <54879050+daikideal@users.noreply.github.com>
2020-07-06 22:59:17 +0900

    Add: インタラクティブなコンポーネントを作る
    - React に <button> がクリックされたら常に再レンダーするよう伝える
    - データ更新のあと、この Square の this.state.value は 'X' になっていますので、盤面に X と表示される

--- react-tutorial/src/index.tsx
+++ react-tutorial/src/index.tsx
@@ -6,10 +6,20 @@ interface SquareProps {
   value: number
 }
 class Square extends React.Component<SquareProps> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
